afbf36e06a12fbb0d33c69fb9c4b8134aeb0c510
AngelBarrios <angelefrenbarriosromero234@gmail.com>
2024-04-08 21:30:53 -0300

    agregando tickets, DTO y current

--- src/DAO/DB/userManager.js
+++ src/DAO/DB/userManager.js
@@ -1,23 +1,41 @@
-import Users from "../models/user.model.js"
+import Users from "../models/user.model.js";
+import UserDTO from "../../DTO/UserDTO.js";
 
+class UserManager {
+  async getCurrentUser(userId) {
+    const user = await UserModel.findById(userId);
+    if (!user) {
+      throw new Error("User not found");
+      const { username, email, roles } = user;
+      return new UserDTO(username, email, roles);
+    }
+  }
+}
 class UsersDAO {
+  static async getUserByEmail(email) {
+    return await Users.findOne({ email });
+  }
 
-    static async getUserByEmail(email){
-        return await Users.findOne({email})
-    }
+  static async getUserByCreds(email, password) {
+    return await Users.findOne({ email, password });
+  }
 
-    static async getUserByCreds(email, password) {
-        return await Users.findOne({email, password})
-    }
+  static async insert(first_name, last_name, age, email, password) {
+    return await new Users({
+      first_name,
+      last_name,
+      age,
+      email,
+      password,
+    }).save();
+  }
 
-    static async insert(first_name, last_name, age, email, password) {
-        return await new Users({first_name, last_name, age, email, password}).save()
-    }
-
-    static async getUserByID(id) {
-        return await Users.findOne({_id:id},{first_name:1, last_name:1, age:1, email:1}).lean()
-    }
-
+  static async getUserByID(id) {
+    return await Users.findOne(
+      { _id: id },
+      { first_name: 1, last_name: 1, age: 1, email: 1 }
+    ).lean();
+  }
 }
 
-export default UsersDAO
+export {UsersDAO, UserManager};
--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -20,11 +20,13 @@ router.get("/ping", (req, res) => {
   res.send("pong");
 });
 
-router.get("/api/sessions/current", (req, res) => {
-  if (req.isAuthenticated()) {
-    res.json({ user: req.user });
-  } else {
-    res.json({ user: null });
+router.get("/current", async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const currentUser = await userManager.getCurrentUser(userId);
+    res.json(currentUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 // vistas
@@ -378,6 +380,56 @@ router.put("/api/carts/:cid", async (req, res) => {
   } catch (error) {
     console.error("Error updating cart:", error.message);
     res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+});
+//////////////////////////Ticket//////////////////////////
+router.post("/carts/:cid/purchase", async (req, res) => {
+  const cartId = req.params.cid;
+  try {
+    const cart = await cartManager.getCart(cartId);
+    if (!cart) {
+      return res
+        .status(404)
+        .send(`Error 404: Cart with ID ${cartId} not found.`);
+    }
+    const productsNotPurchased = [];
+    for (const item of cart.items) {
+      const product = await productManager.getProductById(item.productId);
+      if (!product) {
+        return res
+          .status(404)
+          .send(`Error 404: Product with ID ${item.productId} not found.`);
+      }
+      if (product.stock >= item.quantity) {
+        product.stock -= item.quantity;
+        await product.save();
+      } else {
+        productsNotPurchased.push(item.productId);
+      }
+    }
+    cart.items = cart.items.filter(
+      (item) => !productsNotPurchased.includes(item.productId)
+    );
+    cart.total = cart.items.reduce(
+      (total, item) => total + item.quantity * item.price,
+      0
+    );
+    await cart.save();
+
+    const ticket = await ticketManager.createTicket({
+      code: generateUniqueCode(),
+      amount: cart.total,
+      purchaser: req.session.user.email,
+      products: cart.items.map((item) => ({
+        productId: item.productId,
+        quantity: item.quantity,
+      })),
+      productsNotPurchased: productsNotPurchased
+    });
+
+    return res.status(200).json({ status: "success", ticket, productsNotPurchased });
+  } catch (error) {
+    return res.status(500).send(`Error: ${error.message}`);
   }
 });
 
